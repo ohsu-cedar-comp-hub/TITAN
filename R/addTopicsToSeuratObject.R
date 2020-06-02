0e92c4f59c1e629ca52f4b716322451846655425
adoe21 <doe@RNB01632.ohsum01.ohsu.edu>
2020-06-02 16:38:09 -0700

    continuing vignette

--- R/addTopicsToSeuratObject.R
+++ R/addTopicsToSeuratObject.R
@@ -23,3 +23,32 @@ addTopicsToSeuratObject <- function(model, Object) {
   Object@meta.data   <- cbind(Object@meta.data, modelMat)
   return(Object)
 } 
+
+
+
+#' Adds topic information to SingleCellExperiment metadata
+#'
+#'
+#' This function adds the topic-document information as individual columns to the SingleCellExperiment metadata for each topic
+#'
+#' @param model LDA model output
+#' @param Object SCE object containing the data the model was created with.
+#'
+#' @examples
+#' addTopicsToSCE(LDAmodel, SCE)
+#'
+#' @return SingleCellExperiment object with updated metadata
+#'
+#'
+#' @export
+#'
+#' @import SingleCellExperiment
+
+addTopicsToSCE <- function(model, Object) {
+  modelMat           <- t(scale(model$document_expects, center=TRUE, scale=TRUE))
+  modelMat           <- split(modelMat, rep(1:ncol(modelMat), each = nrow(modelMat)))
+  #rownames(modelMat) <- paste(1:ncol(Object), colnames(Object), sep="_")
+  names(modelMat) <- paste("Topic", 1:length(modelMat), sep="_")
+  Object@colData@listData   <- c(Object@colData@listData, modelMat)
+  return(Object)
+} 
