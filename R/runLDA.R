55fc32060b39de34c7d4bd1d2ac7e1b4b221b4e7
adoe21 <doe@RNB01632.ohsum01.ohsu.edu>
2020-06-23 13:45:28 -0700

    combining parallelrunLDA and runLDA

--- R/runLDA.R
+++ R/runLDA.R
@@ -5,12 +5,15 @@
 #'
 #' @param Object Object containing the data the model was created with.
 #' @param normalizationMethod Normalization method used by Seurat NormalizeData. Options are CLR, LogNormalize and RC.
-#' @param ntopics Number of topics to be used in the model
+#' @param ntopics Number of topics to be used in the model. If parallel == TRUE, a vector of topics to run should be inputted
 #' @param alpha the value for alpha in the LDA model
 #' @param beta the value for beta in the LDA model
 #' @param varFeatures the number of variable features to use in the LDA model. The more features that are used, the slower the model will run and the more noise that will be introduced, but the model will be more complete in representing your entire dataset.
 #' @param iterations the number of iterations used when learning the LDA model.
 #' @param burnin number of iterations to run to allow the model to learn before calculating certain statistics. Models start at random points, so this allows model to get closer to the fit before certain statistics are calculated.
+#' @param parallel if TRUE, will run multiple models in parallel
+#' @param outDir if parallel = TRUE, the output directory for the multiple models
+#' @param cores Number of cores to use, only applicable if parallel = TRUE
 #' @param seed.number random integer to set seed
 #'
 #'
@@ -24,6 +27,7 @@
 #'
 #' @import Seurat
 #' @import lda
+#' @import parallel
 #' @import SingleCellExperiment
 
 
@@ -35,6 +39,9 @@ runLDA <- function(Object,
                    iterations = 500,
                    burnin = 250,
                    seed.number = 8,
+                   parallel = F,
+                   outDir = NULL,
+                   cores = 1,
                    normalizationMethod = "CLR") {
 
   ## Set seed
@@ -45,19 +52,12 @@ runLDA <- function(Object,
     Object        <- NormalizeData(Object, assay = "RNA", normalization.method = normalizationMethod)
     Object        <- FindVariableFeatures(Object, assay = "RNA", nfeatures = varFeatures)
     Object.sparse <- GetAssayData(Object, slot = "data",assay = "RNA")
-    Object.sparse <- Object[VariableFeatures(Object, assay = "RNA"),]
+    Object.sparse <- Object.sparse[VariableFeatures(Object, assay = "RNA"),]
 
   #convert data into the proper input format for lda.collapsed.gibbs.sampler
     data.use      <- Matrix::Matrix(Object.sparse, sparse = T)
-  } else if (class(Object) == "SingleCellExperiment") {
-    normalized_sce <- NormalizeData(assay(Object, "counts"), normalization.method = normalizationMethod)
-    varFeats <- FindVariableFeatures(normalized_sce)
-    varFeats$gene <- rownames(varFeats)
-    varFeats <- top_n(varFeats, 5000, vst.variance.standardized)
-
-    data.use <- Matrix::Matrix(normalized_sce[varFeats$gene,], sparse = T)
   } else {
-    message("Object must be of class singleCellExpiriment or Seurat")
+    message("Object must be of class Seurat")
   }
 
   data.use      <- data.use * 10
@@ -74,17 +74,27 @@ runLDA <- function(Object,
   cellList      <- lapply(cellList, function(x) {colnames(x) <- Genes[x[1,]+1];x})
 
   #Run model
-  Model <- lda.collapsed.gibbs.sampler(
-    cellList,
-    ntopics,
-    Genes,
-    num.iterations=iterations,
-    alpha=alpha,
-    eta=beta,
-    compute.log.likelihood = TRUE,
-    burnin=burnin)[-1]
-
-  return(Model)
+  model_maker <- function(topics) {
+    selected.Model <- lda.collapsed.gibbs.sampler(
+      cellList,
+      topics,
+      Genes,
+      num.iterations=iterations,
+      alpha=alpha,
+      eta=beta,
+      compute.log.likelihood=TRUE,
+      burnin=burnin)[-1]
+    if (parallel) {
+      saveRDS(selected.Model, paste0(outDir, "/Model_", as.character(topics), "topics.rds"))
+    }
+  }
+  if (parallel) {
+    mclapply(ntopics, model_maker, mc.cores = cores)
+  }
+  else {
+    Model <- model_maker(ntopics)
+    return(Model)
+  }
 }
 
 
